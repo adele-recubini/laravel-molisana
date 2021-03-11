34edeeeeb4cf193908c6bf181ef292eec9d1d856
adele <adele.r87@gmail.com>
2021-03-11 18:24:16 +0100

    modifica

--- resources/views/dettagliComponenti/header.blade.php
+++ resources/views/dettagliComponenti/header.blade.php
@@ -4,7 +4,8 @@
         $lista = ['Home', 'Prodotti', 'News'];
     @endphp
 
-    <img src="https://www.lamolisana.it/wp-content/uploads/2020/04/marchio-sito-test.png" alt="">
+    <img src="img/logo.png" alt="">
+
     <nav class="header">
         @foreach ($lista as $link)
             <div><a href="#">{{$link}}</a></div>
